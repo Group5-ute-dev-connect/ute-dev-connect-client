b14884faec7fbcddd185d63881694fad5458b384
Võ Trí Hiệu <hieu981.vn@gmail.com>
2026-05-13 00:53:13 +0700

    Add sample Home page and Navbar;
    Introduce a new Home component, and a new Navbar component with placeholder login/signup actions and a link to Forgot Password. .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import ForgotPassword from './pages/auth/ForgotPassword'
+import Home from './Home'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/auth/forgot-password" element={<ForgotPassword />} />
-        {/* Tạm thời chuyển hướng các route khác về forgot password để test */}
-        <Route path="*" element={<Navigate to="/auth/forgot-password" replace />} />
+        {/* Route mặc định: Điều hướng về trang chủ */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
