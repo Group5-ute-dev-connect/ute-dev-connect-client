6e958bb1254c45289da08465d9975f85d26006bd
Khanh <Quockhanh6310@gmail.com>
2026-05-12 20:36:24 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-200 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-600">
+        Khởi tạo Frontend thành công!
+      </h1>
+    </div>
+  )
+}
+
+export default App
